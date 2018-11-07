254c92d52e6e8f02b3a44694f38af43e32a965ee
Rodrigo Zavala Zevallos <rodizz@hotmail.com>
2018-11-07 20:50:17 +0100

    new game.sh

--- game.sh
+++ game.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 clear
-#
-#Test arguments
-#
+
 if [ $# -gt 1 ]; then
   echo Has introducido más de un argumento
   exit 1
@@ -21,6 +19,7 @@ if !(test -f "conf.cfg"); then
   exit 1
 fi
 
+#-----------ARGUMENTOS------------#
 temp=(`tail -1 conf.cfg|cut -d "=" -f 2`)
 temp1=(`head -1 conf.cfg|cut -d "=" -f 2`)
 temp2=(`head -2 conf.cfg|tail -1 |cut -d "=" -f 2`)
@@ -38,115 +37,67 @@ if !(test -f $ubiEst/$ficEst); then
   echo Estadisticas=$ubiEst/$ficEst>>conf.cfg
   echo Se ha creado el fichero estadisticas.txt
 fi
-#
-#Comienzo de la funcion menu
-#
+
+#--------------MENU---------------#
 g=0
 menu(){
     temp=(`tail -1 conf.cfg|cut -d "=" -f 2`) 
     temp1=(`head -1 conf.cfg|cut -d "=" -f 2`) #numcolores
     temp2=(`head -2 conf.cfg|tail -1 |cut -d "=" -f 2`) #segundos
-clear
-#
-echo "         Saimon v 1.0
-"
-echo "========================="
-echo "J) JUGAR"
-echo "C) Configuracion"
-echo "E) Estadisticas"
-echo "S) SALIR"
-echo ""
-echo "Saimon, Introduzca una opcion >>"
+    clear
+    echo "\t\tSaimon v 1.0\t\t"
+    echo "========================="
+    echo "J) JUGAR"
+    echo "C) Configuracion"
+    echo "E) Estadisticas"
+    echo "S) SALIR"
+    echo ""
+    echo "Saimon, Introduzca una opcion >>"
 
-read opcion
-case "$opcion" in
- j|J)
-  clear
-  game;;
- c|C)
-  config
-  cont;;
- e|E)
-  statistics
-  cont;;
- s|S)
-  echo Has salido del menú
-  exit 0;;
- *)
-  echo Has introducido mal la opción, prueba de nuevo
-  cont;;
-esac
+    read opcion
+    case "$opcion" in
+    j|J)
+        clear
+        game;;
+    c|C)
+        config
+        cont;;
+    e|E)
+        statistics
+        cont;;
+    s|S)
+        echo Has salido del menú
+        exit 0;;
+    *)
+        echo Has introducido mal la opción, prueba de nuevo
+        cont;;
+    esac
 }
-#
-#Final de la funcion menu y comienzo de la funcion juego
-#
-game(){
-#Comprobacion numero de posibilidades y segundos
-echo Numero de posibilidade=$temp1
-echo Numero de segundos=$temp2
- #
- #Variables
- #
- aciertos=0
 
+#-----------JUEGO------------#
+game (){
+    echo Numero de posibilidade=$temp1
+    echo Numero de segundos=$temp2
 
- while(aciertos<20){
-#Crear cadena para (ravz mostrar,esperar y guardar)
+    aciertos = 0
+    while(aciertos<20){
+        echo Introduce la secuencia:
+        read
+        clear
+        if [ "${t}" = "$(echo ${t}|egrep '^[0-9]*$')" ];then
+            let tries--
+            let j--
+            echo "No se permiten números"
+            continue
+        fi
+    }
+}
 
-
-echo Introduce la secuencia:
-read 
-clear
-
-
- if [ "${t}" = "$(echo ${t}|egrep '^[0-9]*$')" ];then
-        let tries--
-        let j--
-	echo "No se permiten números"
-	continue
-
-
-
-
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#
-#End of fail function and begining of askExit function
-#
+#-----------JUEGO------------#
 cont(){
-   echo ""
-   echo \"Pulsa INTRO para continuar\"
-   read;
-     menu
+    echo ""
+    echo \"Pulsa INTRO para continuar\"
+    read;
+    menu
 }
-#
-#End of cont function
-#
-menu
+smenu
